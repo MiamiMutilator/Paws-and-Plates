260930e15d756edb33a6920ec13662bdfd0965cc
alopez0213122 <alopez0213122@gmail.com>
2025-10-15 15:08:06 -0400

    bug fix

--- Assets/Scripts/Blender.cs
+++ Assets/Scripts/Blender.cs
@@ -59,61 +59,68 @@ public class Blender : MonoBehaviour
         {
             Banana = true;
             Debug.Log("Banana added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Coconut"))
         {
             Coconut = true;
             Debug.Log("Coconut added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("CoffeeBean"))
         {
             coffeeBean = true;
             Debug.Log("CoffeeBean added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Ice"))
         {
             Ice = true;
             Debug.Log("Ice added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Mango"))
         {
             Mango = true;
             Debug.Log("Mango added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Matcha"))
         {
             Matcha = true;
             Debug.Log("Matcha added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Milk"))
         {
             Milk = true;
             Debug.Log("Milk added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Peach"))
         {
             Peach = true;
             Debug.Log("Peach added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Pineapple"))
         {
             Pineapple = true;
             Debug.Log("Pineapple added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Strawberry"))
         {
             Strawberry = true;
             Debug.Log("Strawberry added!");
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Vanilla"))
         {
             Vanilla = true;
             Debug.Log("Vanilla added!");
+            Destroy(collision.gameObject);
         }
-        
-
-
-
     }
 
 
